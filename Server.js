b1a33b7bef4a51376c26618a1dc3c9703137ec87
Mehran <mehranbhat2001@gmail.com>
2025-03-04 14:00:36 +0530

    changes

--- Server.js
+++ Server.js
@@ -1,21 +1,13 @@
 const express = require('express');
 const app = express();
 const env = require('dotenv').config();
-const cors = require('cors');
+
 const router = require('./routes/contactRoutes');
 const router2 = require('./routes/userRoutes');
 const errors = require('./middleware/errorHandler');
 const connectDb = require('./config/db');
 app.use(express.json());
 connectDb();
-app.use(
-  cors({
-    origin: 'http://localhost:4081', // Frontend URL
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
-  })
-);
 
 app.use('/v1', router);
 app.use('/v1/user', router2);
